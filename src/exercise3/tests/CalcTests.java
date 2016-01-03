a5124d977925b78cd164e28beb7575a4be9d610b
Khodl <khodls@msoe.edu>
2016-01-02 18:15:06 -0600

    a few of the initial tests for CalculatorTests

--- src/exercise3/tests/CalcTests.java
+++ src/exercise3/tests/CalcTests.java
@@ -5,15 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import exercise3.CalculatorInterface;
+import exercise3.Calculator;
 
 public class CalcTests {
+	
+	CalculatorInterface calc;
 
 	@Before
 	public void setUp() throws Exception {
+		calc = new Calculator();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
