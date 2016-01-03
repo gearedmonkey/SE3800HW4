5bdfb3f4a65b8b37e6ed39b79ae56603cda83bf6
Khodl <khodls@msoe.edu>
2016-01-02 18:16:38 -0600

    the actual file, previous commit had not saved fully yet

--- src/exercise3/tests/CalcTests.java
+++ src/exercise3/tests/CalcTests.java
@@ -2,6 +2,9 @@ package exercise3.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,10 +14,17 @@ import exercise3.Calculator;
 public class CalcTests {
 	
 	CalculatorInterface calc;
+	List<Integer> list;
+	double DELTA = .001;
 
 	@Before
 	public void setUp() throws Exception {
 		calc = new Calculator();
+		list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
 	}
 
 	@After
@@ -23,8 +33,33 @@ public class CalcTests {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() {
+		int expected = 10;
+		int result = calc.add(list);
+		assertEquals(expected, result, DELTA);
 	}
 
+	@Test
+	public void testSubtract() {
+		int expected = -8;
+		int result = calc.subtract(list);
+		assertEquals(expected, result,DELTA);
+	}
+	
+	@Test
+	public void testProduct() {
+		int expected = 24;
+		int result = calc.product(list);
+		assertEquals(expected, result, DELTA);
+	}
+	
+	@Test
+	public void testQuotient() {
+		
+	}
+	
+	@Test
+	public void testDiffSum() {
+		fail("Not yet implemented");
+	}
 }
