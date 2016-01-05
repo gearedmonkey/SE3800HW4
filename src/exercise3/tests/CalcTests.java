73d2b627ef503e001e6e2a460712e223030ea674
Khodl <khodls@msoe.edu>
2016-01-04 20:47:03 -0600

    added more tests to CalcTests.java

--- src/exercise3/tests/CalcTests.java
+++ src/exercise3/tests/CalcTests.java
@@ -10,12 +10,22 @@ import org.junit.Test;
 import exercise3.CalculatorInterface;
 import exercise3.Calculator;
 
+
+/**
+ * Junit test suite for Calculator.java
+ * @author khodls
+ *
+ */
 public class CalcTests {
 	
 	CalculatorInterface calc;
 	List<Integer> list;
 	double DELTA = .001;
 
+	/**
+	 * Create a calculator instance and a list that contains {1,2,3,4} to be used in the tests
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		calc = new Calculator();
@@ -32,12 +42,39 @@ public class CalcTests {
 		int result = calc.add(list);
 		assertEquals(expected, result, DELTA);
 	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testAddNull(){
+		calc.add(null);
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testAddOverflow(){
+		ArrayList<Integer> bigList = new ArrayList<Integer>();
+		 bigList.add(Integer.MAX_VALUE);
+		 bigList.add(10);
+		 calc.add(bigList);
+	}
 
 	@Test
 	public void testSubtract() {
 		int expected = -8;
 		int result = calc.subtract(list);
 		assertEquals(expected, result,DELTA);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testSubtractNull(){
+		calc.subtract(null);
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testSubtractOverflow(){
+		ArrayList<Integer> bigList = new ArrayList<Integer>();
+		bigList.add(Integer.MAX_VALUE);
+		bigList.add(Integer.MAX_VALUE);
+		bigList.add(Integer.MAX_VALUE);
+		calc.subtract(bigList);
 	}
 	
 	@Test
@@ -47,12 +84,27 @@ public class CalcTests {
 		assertEquals(expected, result, DELTA);
 	}
 	
+	@Test(expected = NullPointerException.class)
+	public void testProductNull(){
+		calc.product(null);
+	}
+	
+	@Test(expected = ArithmeticException.class) 
+	public void testProductOverflow(){
+		ArrayList<Integer> bigList = new ArrayList<Integer>();
+		bigList.add(Integer.MAX_VALUE);
+		bigList.add(Integer.MAX_VALUE);
+		bigList.add(Integer.MIN_VALUE);
+		calc.product(bigList);
+	}
+	
 	@Test
 	public void testQuotient() {
 		int expected = 0;
 		int result = calc.quotient(list);
 		assertEquals(expected, result, DELTA);
 	}
+	
 	@Test(expected = ArithmeticException.class)
 	public void testZeroQuotient(){
 		ArrayList<Integer> nums = new ArrayList<Integer>();
@@ -79,5 +131,18 @@ public class CalcTests {
 		int expected = -8;
 		int result = calc.sumDifference(list, nums);
 		assertEquals(expected, result, DELTA);
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testDiffSumOverFlow(){
+		ArrayList<Integer> bigList = new ArrayList<Integer>();
+		bigList.add(Integer.MAX_VALUE);
+		bigList.add(Integer.MAX_VALUE);
+		calc.sumDifference(bigList, list);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testDiffSumOneListNull(){
+		calc.sumDifference(list, null);
 	}
 }
