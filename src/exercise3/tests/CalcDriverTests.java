addc2063c9cba153de3eb204221b76316cb5e144
Jordan Longato <longatoj@msoe.edu>
2016-01-06 15:10:49 -0600

    Added template Junit code for testing the system.out for the Driver tests

---
+++ src/exercise3/tests/CalcDriverTests.java
@@ -0,0 +1,32 @@
+package exercise3.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalcDriverTests {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	}
+
+	@Test
+	public void out() {
+		System.out.print("hello");
+		assertEquals("hello", outContent.toString());
+	}
+
+}
